c91646929fa2c111ca632b374e2cfb5e3cf7c1bc
andijayawizard <andijayawizard@gmail.com>
2024-02-19 17:40:39 +0700

    19022024

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "js_simple_crud",
+    PASSWORD: "#$34ERdfcv",
+    DB: "js_simple_crud",
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
